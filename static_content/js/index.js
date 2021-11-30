50bb8230686bbac0f4948a12d98582112548a9a0
ABNER THIAGO DA SILVA GUIMARAES <abner.guimaraes@bradesco.com.br>
2021-11-29 23:39:41 -0300

    Versao 1.0

--- static_content/js/index.js
+++ static_content/js/index.js
@@ -1,9 +1,39 @@
-var linkedIn = document.getElementById("lknLinked");
-var mail     = document.getElementById("lknMail");
+var docWidth = document.documentElement.offsetWidth;
+[].forEach.call(
+  document.querySelectorAll('*'),
+  function(el) {
+    if (el.offsetWidth > docWidth) {
+      console.log(el);
+    }
+  }
+);
 
-linkedIn.addEventListener("click", function(){
-    window.open("https://www.linkedin.com/in/abnerguimaraes/", "_blank")
-});
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("btnWhats").style.display = "flex";
+        document.getElementById("btnWhats").classList.remove("btn--green");
+        document.getElementById("btnWhats").classList.add("btn--green__fixed");
+        document.getElementById("btnWhats").style.top = document.documentElement.scrollTop + 16 + "px";
+        console.log(document.documentElement.scrollTop);
+    } else {
+        document.getElementById("btnWhats").style.display = "flex";
+        document.getElementById("btnWhats").classList.remove("btn--green__fixed");
+        document.getElementById("btnWhats").classList.add("btn--green");
+        document.getElementById("btnWhats").style.top = null;
+    }
+    
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+var mail = document.getElementById("money");
 
 mail.addEventListener("click", function(){
     window.open("mailto:guimaraesabner@gmail.com", "_blank");
